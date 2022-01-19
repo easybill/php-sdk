0ee0338cd21aa4c98f35001c7aa03a903f1ee326
Patrick Romowicz <patrick@romowicz.de>
2022-01-19 20:35:20 +0100

    Update example

--- examples/with_models/customers_create_and_update.php
+++ examples/with_models/customers_create_and_update.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Easybill\SDK\Client;
 use Easybill\SDK\Endpoint;
@@ -10,6 +10,7 @@ use Easybill\SDK\Models\Customer;
 
 $client = new Client(new Endpoint(getenv('API_KEY')));
 
+## Create Customer
 $customerCreate = new Customer();
 $customerCreate->setFirstName('Foo');
 $customerCreate->setLastName('Bar');
@@ -21,3 +22,13 @@ $result = $client->request('POST', 'customers', $customerCreate->toArray());
 $customer = new Customer($result);
 
 var_dump($customer);
+
+## Update Customer
+$customerUpdate = new Customer();
+$customerUpdate->setCompanyName('FooBar Ltd.');
+
+$result = $client->request('PUT', "customers/{$customer->getId()}", $customerUpdate->toArray());
+
+$customer = new Customer($result);
+
+var_dump($customer->getCompanyName());
